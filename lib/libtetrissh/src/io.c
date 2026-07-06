e72dacff2a98239c900aedf1a316d08a601f51f1
DarK_Sanjan <sanjankrishna007@protonmail.com>
2026-07-06 16:29:41 +0800

    [libtetrissh] add exact socket io

--- lib/libtetrissh/src/io.c
+++ lib/libtetrissh/src/io.c
@@ -1,33 +1,77 @@
 #include "internal.h"
+#include <errno.h>
+#include <sys/socket.h>
 
 t_tsh_io_result	tsh_read_exact(int fd, void *buf, size_t len)
 {
-	(void)fd;
-	(void)buf;
-	(void)len;
-	return (TSH_IO_ERR);
+	unsigned char	*out;
+	size_t			done;
+	ssize_t			n;
+
+	out = buf;
+	done = 0;
+	while (done < len)
+	{
+		n = recv(fd, out + done, len - done, 0);
+		if (n == 0)
+			return (done == 0 ? TSH_IO_EOF : TSH_IO_ERR);
+		if (n < 0)
+		{
+			if (errno == EINTR)
+				continue;
+			return (TSH_IO_ERR);
+		}
+		done += (size_t)n;
+	}
+	return (TSH_IO_OK);
 }
 
 int	tsh_write_exact(int fd, const void *buf, size_t len)
 {
-	(void)fd;
-	(void)buf;
-	(void)len;
-	return (-1);
+	const unsigned char	*in;
+	size_t				done;
+	ssize_t				n;
+
+	in = buf;
+	done = 0;
+	while (done < len)
+	{
+		n = send(fd, in + done, len - done, 0);
+		if (n <= 0)
+		{
+			if (n < 0 && errno == EINTR)
+				continue;
+			return (-1);
+		}
+		done += (size_t)n;
+	}
+	return (0);
 }
 
 t_tsh_io_result	tsh_read_u32(int fd, uint32_t *value)
 {
-	(void)fd;
-	(void)value;
-	return (TSH_IO_ERR);
+	unsigned char	buf[4];
+	t_tsh_io_result	res;
+
+	if (value == NULL)
+		return (TSH_IO_ERR);
+	res = tsh_read_exact(fd, buf, sizeof(buf));
+	if (res != TSH_IO_OK)
+		return (res);
+	*value = ((uint32_t)buf[0] << 24) | ((uint32_t)buf[1] << 16)
+		| ((uint32_t)buf[2] << 8) | (uint32_t)buf[3];
+	return (TSH_IO_OK);
 }
 
 int	tsh_write_u32(int fd, uint32_t value)
 {
-	(void)fd;
-	(void)value;
-	return (-1);
+	unsigned char	buf[4];
+
+	buf[0] = (unsigned char)((value >> 24) & 0xffu);
+	buf[1] = (unsigned char)((value >> 16) & 0xffu);
+	buf[2] = (unsigned char)((value >> 8) & 0xffu);
+	buf[3] = (unsigned char)(value & 0xffu);
+	return (tsh_write_exact(fd, buf, sizeof(buf)));
 }
 
 void	tsh_u64_be(uint64_t value, unsigned char out[8])
