f5bed80ac6b8c5c7f7156a0dc9a52fd8e8d21ce9
Thong Zi Qi <ziqi.thong@gmail.com>
2026-07-30 03:36:23 +0800

    feat - basic files for tetrislogd

---
+++ src/tetrislogd/include/tetrislogd.h
@@ -0,0 +1,4 @@
+# ifndef TETRISLOGD_H
+# define TETRISLOGD_H
+
+# endif
---
+++ src/tetrislogd/src/main.c
@@ -0,0 +1,8 @@
+#include "tetrislogd.h"
+
+int main(int argc, char **argv)
+{
+    (void) argc;
+    (void) argv;
+    return 0;
+}
